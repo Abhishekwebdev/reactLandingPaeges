0cb1cc3d9976c5eb3b9decd007c9e28e75b56330
Abhishek Patel <Abhi882731@gmail.com>
2021-08-02 03:50:55 -0700

    Updates

---
+++ precache-manifest.e66d4af13efc68deb20edbdc51fcd08d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680b68156526a6d9178b287fd4cf4c3a",
+    "url": "/reactLandingPaeges/index.html"
+  },
+  {
+    "revision": "f38dcec4d53b8826aca1",
+    "url": "/reactLandingPaeges/static/css/2.9f603e7f.chunk.css"
+  },
+  {
+    "revision": "7494327b88be62ae4860",
+    "url": "/reactLandingPaeges/static/css/main.042e7086.chunk.css"
+  },
+  {
+    "revision": "f38dcec4d53b8826aca1",
+    "url": "/reactLandingPaeges/static/js/2.bf2893c5.chunk.js"
+  },
+  {
+    "revision": "7a94d9fc5e6a474ec6646e0785b38fc0",
+    "url": "/reactLandingPaeges/static/js/2.bf2893c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7494327b88be62ae4860",
+    "url": "/reactLandingPaeges/static/js/main.44a68794.chunk.js"
+  },
+  {
+    "revision": "f4de38572b4a10f1ebd2",
+    "url": "/reactLandingPaeges/static/js/runtime-main.cc44004b.js"
+  },
+  {
+    "revision": "3f5497c84e25fbba4f6f21c65936d6a5",
+    "url": "/reactLandingPaeges/static/media/4.3f5497c8.png"
+  },
+  {
+    "revision": "ca7034cc7f576e59224976c74b3bab9a",
+    "url": "/reactLandingPaeges/static/media/bg-2.ca7034cc.jpg"
+  },
+  {
+    "revision": "fda2aaedaa6d26ab141b5884fa3c1cc6",
+    "url": "/reactLandingPaeges/static/media/bg-3.fda2aaed.jpg"
+  },
+  {
+    "revision": "74225eb9f4aaf5824020cc11d64fce12",
+    "url": "/reactLandingPaeges/static/media/bg.74225eb9.jpg"
+  },
+  {
+    "revision": "0d16c744182f24f28979da38f46d1ab4",
+    "url": "/reactLandingPaeges/static/media/home-bg.0d16c744.jpg"
+  },
+  {
+    "revision": "3bd364e5b4f5c3e57a7376091996d4b4",
+    "url": "/reactLandingPaeges/static/media/materialdesignicons-webfont.3bd364e5.eot"
+  },
+  {
+    "revision": "3ef6639a4cce5b903e4031b1b0102675",
+    "url": "/reactLandingPaeges/static/media/materialdesignicons-webfont.3ef6639a.ttf"
+  },
+  {
+    "revision": "9b9f2c447d27a622fcb78f6b7f38a095",
+    "url": "/reactLandingPaeges/static/media/materialdesignicons-webfont.9b9f2c44.woff2"
+  },
+  {
+    "revision": "eec7f0f7c8944b878af8fb7fcc091ade",
+    "url": "/reactLandingPaeges/static/media/materialdesignicons-webfont.eec7f0f7.woff"
+  },
+  {
+    "revision": "f89ba2d89e5710c9ff4d03829963cf2d",
+    "url": "/reactLandingPaeges/static/media/materialdesignicons-webfont.f89ba2d8.svg"
+  },
+  {
+    "revision": "0786a49284357f97ce22807a140dfec1",
+    "url": "/reactLandingPaeges/static/media/mobirise.0786a492.svg"
+  },
+  {
+    "revision": "2d2c45f26abdcc4d04e7ef0f20bb5e0f",
+    "url": "/reactLandingPaeges/static/media/mobirise.2d2c45f2.eot"
+  },
+  {
+    "revision": "3995447d39b45754481ed11b345253d1",
+    "url": "/reactLandingPaeges/static/media/mobirise.3995447d.ttf"
+  },
+  {
+    "revision": "77a21496ccb320a88fbdb06fb3cb56a3",
+    "url": "/reactLandingPaeges/static/media/mobirise.77a21496.woff"
+  },
+  {
+    "revision": "a368359b99de199160b26b68b7c5088a",
+    "url": "/reactLandingPaeges/static/media/mockup1.a368359b.png"
+  },
+  {
+    "revision": "6696c592b7cf8ebeb809a06f9595b0df",
+    "url": "/reactLandingPaeges/static/media/mockup2.6696c592.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/reactLandingPaeges/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/reactLandingPaeges/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/reactLandingPaeges/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/reactLandingPaeges/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ece5957f178fcc825debf250b9c74b42",
+    "url": "/reactLandingPaeges/static/media/team-1.ece5957f.png"
+  },
+  {
+    "revision": "14c09c7e24943a9c25c0438a0b90d87f",
+    "url": "/reactLandingPaeges/static/media/team-2.14c09c7e.png"
+  },
+  {
+    "revision": "9ca050de5bcb3c79080268e3339c8bc7",
+    "url": "/reactLandingPaeges/static/media/team-3.9ca050de.png"
+  },
+  {
+    "revision": "28cdf187145479fa03bb6fa5c8a950af",
+    "url": "/reactLandingPaeges/static/media/team-4.28cdf187.png"
+  }
+]);
